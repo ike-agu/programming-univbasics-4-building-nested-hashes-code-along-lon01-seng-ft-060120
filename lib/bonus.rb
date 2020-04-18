d638ca1a9e66ad96fb6aeb3966f55209c002fae4
ikenna agulobi <ikennaww@yahoo.fr>
2020-04-18 18:49:16 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -24,7 +24,7 @@ def bonus
   
    epic_tragedy[:montague][:hero][:status] = "dead"
    
-   epic_tragedy[:capulet][:heroine][:status] = "dead"
+   epic_tragedy[:capulett][:heroine][:status] = "dead"
   
   
   
